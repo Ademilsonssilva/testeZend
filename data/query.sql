499445d394128ee693c6dd89220ed0ec06cc0632
Ademilson <ademilsonssilva1@gmail.com>
2016-08-25 11:32:13 -0300

    query do DB

---
+++ data/query.sql
@@ -0,0 +1,19 @@
+CREATE TABLE banco
+(
+  id serial NOT NULL,
+  codigo character varying(6),
+  descricao character varying(150),
+  CONSTRAINT banco_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE agencia
+(
+  id serial NOT NULL,
+  ban_id integer,
+  nome character varying(50),
+  endereco character varying(100),
+  CONSTRAINT agencia_pkey PRIMARY KEY (id),
+  CONSTRAINT agencia_ban_id_fkey FOREIGN KEY (ban_id)
+      REFERENCES banco (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT
+);
